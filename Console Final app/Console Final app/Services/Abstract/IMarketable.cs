931223fe78d63f0797002c8841ec949cfa084a88
Ayddemiirr <aydamirkr@code.edu.az>
2023-07-30 13:12:50 +0400

    complate interface

--- Console Final app/Console Final app/Services/Abstract/IMarketable.cs
+++ Console Final app/Console Final app/Services/Abstract/IMarketable.cs
@@ -12,5 +12,21 @@ namespace ConsoleApp1.Services.Abstract
     {
         public List<Sales> GetSales();
         public List<Product> GetProducts();
+        public void ReturnProductFromSale(int SaleId, int ProductId, int count);
+        public void AddSales(List<int> ProductIds, List<int> SaleCounts);
+        public void DeleteProduct(int id);
+        public List<Sales> GetSalesForDateInterval(DateTime FirstDate, DateTime LastDate);
+        public void DeleteSale(int SaleId);
+        public List<Sales> GetSalesForDate(DateTime dateTime);
+        public List<Sales> GetSalesForPriceInterval(decimal minPrice, decimal maxPrice);
+        public int AddProduct(string name, decimal price, Categories categories, int count);
+        public Sales GetSalesById(int saleId);
+        public void UpdateProduct(string NewName, decimal NewPrice, int NewCount, Categories NewCategories, int id);
+        public List<Product> GetProductsByCategory(Categories categoryName);
+        public List<Product> GetProductsByName(string name);
+        public List<Product> GetProductsForPriceInterval(decimal minPrice, decimal maxPrice);
+
+
+
     }
 }
--- Console Final app/Console Final app/Services/Concrete/MarketServices.cs
+++ Console Final app/Console Final app/Services/Concrete/MarketServices.cs
@@ -13,6 +13,22 @@ namespace ConsoleApp1.Services.Concrete
     {
         public List<Product> products = new();
         public List<Sales> sales = new();
+        public int AddProduct(string name, decimal price, Categories categories, int count)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new Exception("Name is null!");
+            if (price == null) throw new Exception("price is null!");
+            if (count == null) throw new Exception("count is null!");
 
+
+            Product product = new Product
+            {
+                Name = name,
+                Price = price,
+                Categories = categories,
+                Count = count
+            };
+            products.Add(product);
+            return product.Id;
+        }
     }
 }
